521eb9855e1dc45d42dcc6599aa6b3cae6b095bf
padmini v <padmini.v@perfios.com>
2021-09-29 18:31:43 +0530

    ADDED: Address class to maintain Address attributes like city,state and zip

---
+++ src/main/java/AddressBookSystemGradle/Address.java
@@ -0,0 +1,64 @@
+package AddressBookSystemGradle;
+
+
+public class Address 
+{
+	private Integer addressID;
+	private String city;
+	private String zip;
+	private String state;
+	
+	public Address(Integer addressID,String city,String zip,String state)
+	{
+		this.addressID=addressID;
+		this.city=city;
+		this.zip=zip;
+		this.state=state;
+	}
+	public Address(String city,String zip,String state)
+	{
+		this.city=city;
+		this.zip=zip;
+		this.state=state;
+	}
+	
+	public Integer getAddressID() 
+	{
+		return addressID;
+	}
+	
+	public void setAddressID(Integer addressID) 
+	{
+		this.addressID = addressID;
+	}
+	
+	public String getCity() 
+	{
+		return city;
+	}
+	
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+	
+	public String getZip() 
+	{
+		return zip;
+	}
+	
+	public void setZip(String zip) 
+	{
+		this.zip = zip;
+	}
+	
+	public String getState() 
+	{
+		return state;
+	}
+	
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+}
