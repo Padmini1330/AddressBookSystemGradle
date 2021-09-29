fdca775683bc8510e72bc7c0a99b59edb09a592b
padmini v <padmini.v@perfios.com>
2021-09-29 19:52:38 +0530

    REFACTORED: added methods to read contacts in the given range

--- src/main/java/AddressBookSystemGradle/AddressBookDBService.java
+++ src/main/java/AddressBookSystemGradle/AddressBookDBService.java
@@ -12,26 +12,23 @@ import AddressBookSystemGradle.AddContactDetails.IOService;
 
 public class AddressBookDBService 
 {
-	
-	
-	public static Connection getConnection() throws SQLException 
-    {
-		
-		String jdbcURL = "jdbc:mysql://localhost:3306/AddressBookService?useSSL=false";
-		String userName = "root";
-		String password = "Padmini_1330";
-		Connection connection;
-		
-		System.out.println("Connecting to the database : "+jdbcURL);
-		connection = DriverManager.getConnection(jdbcURL, userName, password);
-		System.out.println("Connection is Succcessfully Established!! "+connection);
-		
-		return connection;
+	AddContactDetails contactDetails=new AddContactDetails();
+	private static AddressBookDBService addressBookDBService;
+	private java.sql.PreparedStatement readContactPreparedStatement;
+	private PreparedStatement contactAddedGivenRange;
+
+	public AddressBookDBService() 
+	{
+
+	}
+
+	public static AddressBookDBService getDBInstance() 
+	{
+		addressBookDBService = new AddressBookDBService();
+		return addressBookDBService;
 	}
 	
-	AddContactDetails contactDetails=new AddContactDetails();
-	public void writeService(String fileName, HashMap<String, Contact> addressBook, IOService ioService) 
-	{
+	public void writeService(String fileName, HashMap<String, Contact> addressBook, IOService ioService) {
 		if (ioService == IOService.CSV_IO)
 			contactDetails.writeToCsvFile(fileName, addressBook);
 		else if (ioService == IOService.FILE_IO)
@@ -49,8 +46,7 @@ public class AddressBookDBService
 		}
 	}
 
-	public void readService(String fileName, HashMap<String, Contact> addressBook, IOService ioService) 
-	{
+	public void readService(String fileName, HashMap<String, Contact> addressBook, IOService ioService) {
 		if (ioService == IOService.CSV_IO)
 			contactDetails.readFromCsvFile(fileName, addressBook);
 		else if (ioService == IOService.FILE_IO)
@@ -67,47 +63,171 @@ public class AddressBookDBService
 			}
 		}
 	}
-	
-	public List<Contact> readContacts(int addressBookID)
+
+	public static Connection getConnection() throws SQLException 
+    {
+		
+		String jdbcURL = "jdbc:mysql://localhost:3306/AddressBookService?useSSL=false";
+		String userName = "root";
+		String password = "Padmini_1330";
+		Connection connection;
+		
+		System.out.println("Connecting to the database : "+jdbcURL);
+		connection = DriverManager.getConnection(jdbcURL, userName, password);
+		System.out.println("Connection is Succcessfully Established!! "+connection);
+		
+		return connection;
+	}
+
+	public List<Contact> readContacts(int addressBookID) 
 	{
-		String sql="SELECT * from ContactDetails where AddressBookID=\""+addressBookID+"\"";
-		List<Contact> contactsList=new ArrayList<Contact>();
-		try
+		ResultSet resultSet;
+		if (readContactPreparedStatement == null)
+			this.preparedStatementToReadContacts();
+		try 
 		{
-			Connection connection=this.getConnection();
-			System.out.println("connected");
-			Statement statement=connection.createStatement();
-			ResultSet resultSet=statement.executeQuery(sql);
+			readContactPreparedStatement.setInt(1, addressBookID);
+			resultSet = readContactPreparedStatement.executeQuery();
+		} 
+		catch (SQLException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
+
+		return getContactList(resultSet);
+	}
+
+	public List<Contact> getContactList(ResultSet resultSet)
+	{
+		List<Contact> contactsList = new ArrayList<Contact>();
+		try 
+		{
 			while(resultSet.next())
 			{
 				contactsList.add(new Contact(resultSet.getInt("AddressBookID"),resultSet.getString("FirstName"), resultSet.getString("LastName"), resultSet.getInt("AddressID"), resultSet.getString("PhoneNumber"),resultSet.getInt("ContactID"), resultSet.getString("Email")));
 			}
-		}
-		catch (Exception e) 
+		} 
+		catch (SQLException e) 
 		{
-			e.printStackTrace();
+			throw new DatabaseException(e.getMessage());
 		}
 		return contactsList;
-	}
-	
-	public void writeAddressBookDB(Contact contact,int addressBookID)
-	{
-		String insertQueryString="INSERT into ContactDetails values("+contact.getAddressBookId()+",\""+contact.getFirstName()+"\",\""+contact.getLastName()+"\",\""+contact.getAddressID()+"\",\""+contact.getPhoneNumber()+"\",\""+contact.getContactId()+"\",\""+contact.getEmail()+"\")";
-		String insertPlaceQueryString="INSERT into Address values("+contact.getAddress().getAddressID()+",\""+contact.getCity()+"\",\""+contact.getState()+"\",\""+contact.getZipCode()+"\")";
-		try 
-		{
-			Connection connection=this.getConnection();
-			System.out.println("connected");
-			Statement statement=connection.createStatement();
-			statement.executeUpdate(insertPlaceQueryString);
-			statement.executeUpdate(insertQueryString);
-			
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
+
 	}
 
+	public Contact writeAddressBookDB(Contact contact, int addressBookID) 
+	{
+		Contact updatedContact;
+		String insertQueryString="INSERT into ContactDetails values("+contact.getAddressBookId()+",\""+contact.getFirstName()+"\",\""+contact.getLastName()+"\",\""+contact.getAddressID()+"\",\""+contact.getPhoneNumber()+"\",\""+contact.getContactId()+"\",\""+contact.getEmail()+"\")";
+		String insertPlaceQueryString="INSERT into Address values("+contact.getAddress().getAddressID()+",\""+contact.getCity()+"\",\""+contact.getState()+"\",\""+contact.getZipCode()+"\")";
+				
+		Connection connection;
+		try 
+		{
+			connection = this.getConnection();
+			connection.setAutoCommit(false);
+		} 
+		catch (Exception e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
+
+		try (Statement statement = connection.createStatement())
+		{
+			statement.executeUpdate(insertPlaceQueryString);
+		} 
+		catch (Exception e) 
+		{
+			try 
+			{
+				connection.rollback();
+			} 
+			catch (SQLException e1) 
+			{
+				throw new DatabaseException(e.getMessage());
+			}
+			throw new DatabaseException(e.getMessage());
+		}
+
+		try (Statement statement = connection.createStatement()) 
+		{
+			statement.executeUpdate(insertQueryString);
+			connection.commit();
+			updatedContact = contact;
+		} 
+		catch (Exception e) 
+		{
+			try 
+			{
+				connection.rollback();
+			} 
+			catch (SQLException sqlException)
+			{
+				throw new DatabaseException(sqlException.getMessage());
+			}
+			throw new DatabaseException(e.getMessage());
+		}
+
+		finally
+		{
+			try
+			{
+				connection.close();
+			} 
+			catch (SQLException e)
+			{
+				throw new DatabaseException(e.getMessage());
+			}
+		}
+		return updatedContact;
+	}
+
+	
+
+	private void preparedStatementToReadContacts() 
+	{
+		try 
+		{
+			Connection connection = this.getConnection();
+			String sql = "SELECT * from ContactDetails where AddressBookID=?";
+			readContactPreparedStatement = connection.prepareStatement(sql);
+		} 
+		catch (Exception e) 
+		{
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	private void preparedStatementToRetriveContactsInRange()
+	{
+        try 
+        {
+            Connection connection = this.getConnection();
+            String query = "SELECT * from ContactDetails where DateAdded between ? and ?";
+            contactAddedGivenRange = connection.prepareStatement(query);
+        }
+        catch (Exception e)
+        {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+	public List<Contact> readContactsAddedInRange(Date startDate, Date endDate) 
+	{
+        if (contactAddedGivenRange == null) 
+        {
+            this.preparedStatementToRetriveContactsInRange();
+        }
+        try 
+        {
+        	contactAddedGivenRange.setDate(1, startDate);
+        	contactAddedGivenRange.setDate(2, endDate);
+            ResultSet resultSet = contactAddedGivenRange.executeQuery();
+            return this.getContactList(resultSet);
+        } 
+        catch (Exception e)
+        {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
 	
 }
