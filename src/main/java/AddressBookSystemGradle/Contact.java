05d59fc9ca3264930710f804ee86f8e2cc598399
padmini v <padmini.v@perfios.com>
2021-09-29 18:34:51 +0530

    REFACTORED: changed constructors and variable names, added getters and setters

--- src/main/java/AddressBookSystemGradle/Contact.java
+++ src/main/java/AddressBookSystemGradle/Contact.java
@@ -7,133 +7,179 @@ import com.opencsv.bean.CsvBindByName;
 public class Contact 
 {
 	
+	public int getContactId() 
+	{
+		return contactID;
+	}
+	public void setContactId(int contactId) 
+	{
+		this.contactID = contactId;
+	}
+	public int getAddressBookId() 
+	{
+		return addressBookID;
+	}
+	public void setAddressBookId(int addressBookId) 
+	{
+		this.addressBookID=addressBookId;
+	}
+	public String getZip() 
+	{
+		return zip;
+	}
+	public void setZip(String zip) 
+	{
+		this.zip = zip;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setEmail(String email) 
+	{
+		this.email = email;
+	}
+
+	@CsvBindByName
+	private int contactID;
+	@CsvBindByName
+	private int addressBookID;
 	@CsvBindByName
 	private String firstName;
 	@CsvBindByName
 	private String lastName;
 	@CsvBindByName
-	private String address;
+	private int addressID;
 	@CsvBindByName
 	private String city;
 	@CsvBindByName
 	private String state;
 	@CsvBindByName
-	private int zip;
+	private String zip;
 	@CsvBindByName
-	private int phoneNumber;
+	private String phoneNumber;
 	@CsvBindByName
 	private String email;
-
-	Contact(String firstName, String lastName, String address, String city, String state, int zip, int phoneNumber,
-			String email) 
+	Address address;
+	Contact(String firstName, String lastName, String city, int addressID, String state, String zip, String phoneNumber,
+			String email)
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
+		this.addressID = addressID;
 		this.phoneNumber = phoneNumber;
 		this.email = email;
+		address=new Address(city, zip, state);
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	Contact(Integer contactId,Integer addressBookId,Integer addressID,String firstName, String lastName, String city, String state, String zip, String phoneNumber,
+			String email) 
+	{
+		this.addressBookID=addressBookId;
+		this.contactID=contactId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.addressID = addressID;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		Address address1=new Address(addressBookID,city,zip,state);
 	}
 
-	public String getFirstName() 
+	public Contact(int addressBookID, String firstName, String lastName, int addressID, String phoneNumber, int contactID, String email) 
 	{
+		this.addressBookID=addressBookID;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.addressID=addressID;
+		this.phoneNumber=phoneNumber;
+		this.contactID=contactID;
+		this.email=email;
+	}
+	public String getFirstName() {
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) 
-	{
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	public String getLastName() 
-	{
+	public String getLastName() {
 		return lastName;
 	}
 
-	public void setLastName(String lastName) 
-	{
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	public String getAddress() 
-	{
-		return address;
+	public int getAddressID() {
+		return addressID;
 	}
 
-	public void setAddress(String address) 
-	{
-		this.address = address;
+	public void setAddressD(int addressID) {
+		this.addressID = addressID;
 	}
 
-	public String getCity() 
-	{
-		return city;
+	public String getCity() {
+		return getAddress().getCity();
 	}
 
-	public void setState(String state) 
-	{
-		this.state = state;
+	public void setState(String state) {
+		this.getAddress().setState(state);
 	}
 
-	public String getState() 
-	{
-		return state;
+	public String getState() {
+		return getAddress().getState();
 	}
 
-	public void setCity(String city) 
-	{
-		this.city = city;
+	public void setCity(String city) {
+		this.getAddress().setCity(city);
 	}
 
-	public int getZipCode() 
-	{
-		return zip;
+	public String getZipCode() {
+		return getAddress().getZip();
 	}
 
-	public void setZipCode(int zip) 
-	{
-		this.zip = zip;
+	public void setZipCode(String zip) {
+		this.getAddress().setZip(zip); 
 	}
 
-	public int getPhoneNumber() 
-	{
+	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(int phoneNumber) 
-	{
+	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
-	public String getEmailId() 
-	{
+	public String getEmailId() {
 		return email;
 	}
 
-	public void setEmailId(String email) 
-	{
+	public void setEmailId(String email) {
 		this.email = email;
 	}
 
-	
 	@Override
-	public boolean equals(Object comparingObject) 
-	{
-		HashMap<String, Contact> addressBook=(HashMap<String, Contact>) comparingObject;
-		if(addressBook.containsKey(this.getFirstName())) 
-		{
+	public boolean equals(Object anotherObject) {
+		HashMap<String, Contact> addressBook = (HashMap<String, Contact>) anotherObject;
+		if (addressBook.keySet().stream().anyMatch(s -> (s.equals(firstName)))) {
 			return true;
 		}
 		return false;
+
 	}
 
 	@Override
-	public String toString() 
-	{
-		return "Contact Details [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
-				+ city + ", state=" + state + ", email=" + email + ", zip=" + zip + ", phoneNumber=" + phoneNumber
-				+ "]";
+	public String toString() {
+		return "First Name: " + getFirstName() + "\n" + "Last Name: " + getLastName() + "\n" + "city :" + getCity()
+				+ "\n" + "Address: " + getAddress() + "\n" + "state: " + getState() + "\n" + "Phone Number: "
+				+ getPhoneNumber() + "\n" + "Email: " + getEmailId();
 	}
+
+
 }	
